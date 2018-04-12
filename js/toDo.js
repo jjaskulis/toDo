a74578d7cb2f2d2b9d3bc8e7d96461e8ca1bdb3a
Justas Jaškulis <jjaskulis@gmail.com>
2018-04-12 16:26:24 +0300

    update0412
    1.filters for tasks added.
    2.changed design

--- js/toDo.js
+++ js/toDo.js
@@ -1,6 +1,6 @@
 'use strict'
 // Initialize Firebase
-let config = {
+const config = {
     apiKey: "AIzaSyBSi2PdmPKx1rIqKtIDP1xUaQw0nWbo92I",
     authDomain: "todo-4de9a.firebaseapp.com",
     databaseURL: "https://todo-4de9a.firebaseio.com",
@@ -9,10 +9,10 @@ let config = {
 
 firebase.initializeApp(config);
 
-let database = firebase.database();
+const database = firebase.database();
 
 database.ref().on('value', function (snapshot) {
-    app.updateTasks(snapshot);
+    todo.refreshTasks(snapshot);
 });
 
 // Vue components
@@ -34,16 +34,35 @@ Vue.component('date-picker', {
 
 
 // Vue instance
-let app = new Vue({
+const todo = new Vue({
     el: "#toDo-app",
     data: {
+        filter: {
+            buttons: {
+                all: {
+                    active: true,
+                    name: "all",
+                    type: "all",
+                },
+                completed: {
+                    active: false,
+                    name: "completed",
+                    type: true,
+
+                },
+                pending: {
+                    active: false,
+                    name: "pending",
+                    type: false,
+                }
+            },
+            active: "all",
+        },
         taskList: [],
-        statusFilter:"",
-        modalVisible: false,
     },
     methods: {
-        updateTasks: function (snapshot) {
-            app.taskList = [];
+        refreshTasks: function (snapshot) {
+            todo.taskList = [];
             snapshot.forEach(function (item) {
                 let task = {
                     id: item.getKey(),
@@ -51,7 +70,7 @@ let app = new Vue({
                     date: item.val().date,
                     status: item.val().status
                 };
-                app.taskList.push(task);
+                todo.taskList.push(task);
             })
         },
         addTask: function () {
@@ -65,20 +84,49 @@ let app = new Vue({
         removeTask: function (id) {
             database.ref(id).remove();
         },
-        updateTask: function (task){
+        updateTask: function (task) {
             let updateTask = {
-                name : task.name,
+                name: task.name,
                 date: task.date,
                 status: task.status
             };
             database.ref(task.id).update(updateTask);
         },
-        toggleStatus: function(task){
-            console.log("something");
-
-            console.log(task);
+        toggleStatus: function (task) {
             task.status = !task.status;
-        }
-    }
+            this.updateTask(task);
+        },
+        taskFilter: function (task) {
+            if (this.filter.active === "all" || this.filter.active === task.status) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+    },
+    computed: {
+        completedQty: function(){
+            let total = 0;
+            this.taskList.forEach(function(task){
+                if(task.status === true){
+                    total++;
+                }
+            })
+            return total;
+        },
+        pendingdQty: function(){
+            let total = 0;
+            this.taskList.forEach(function(task){
+                if(task.status === false){
+                    total++;
+                }
+            })
+            return total;
+
+        },
+}
+
+
 })
 
